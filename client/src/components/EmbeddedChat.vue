80594bbea6d9ce8e4bcd82f56946fd223d3e623b
Max Ripper <max@jovo.tech>
2020-10-22 14:56:17 +0200

    :lipstick: Update chat-window

--- client/src/components/EmbeddedChat.vue
+++ client/src/components/EmbeddedChat.vue
@@ -1,19 +1,6 @@
 <template>
-  <div class="flex flex-col border rounded-xl relative max-h-full">
-    <div
-      v-if="!$client.isInitialized"
-      class="absolute w-full h-full inset-0 flex items-center justify-center bg-black bg-opacity-50 rounded-xl"
-    >
-      <button
-        class="bg-primary w-24 h-24 flex items-center justify-center rounded-full text-white focus:outline-none"
-        @click="initialize"
-      >
-        <play-icon />
-      </button>
-    </div>
-
-    <embedded-chat-header class="rounded-t-xl border-b" />
-    <embedded-chat-body />
+  <div class="flex flex-col border rounded-xl relative max-h-full h-full">
+    <embedded-chat-body class="flex-grow rounded-t-xl" />
     <embedded-chat-footer class="rounded-b-xl border-t" />
   </div>
 </template>
@@ -21,19 +8,12 @@
 <script lang="ts">
 import EmbeddedChatBody from '@/components/EmbeddedChatBody.vue';
 import EmbeddedChatFooter from '@/components/EmbeddedChatFooter.vue';
-import EmbeddedChatHeader from '@/components/EmbeddedChatHeader.vue';
-import { Component, Vue } from 'vue-property-decorator';
 import PlayIcon from 'vue-feather-icons/icons/PlayIcon';
-import { RequestType } from 'jovo-client-web-vue';
+import { Component, Vue } from 'vue-property-decorator';
 
 @Component({
   name: 'embedded-chat',
-  components: { EmbeddedChatFooter, EmbeddedChatBody, EmbeddedChatHeader, PlayIcon },
+  components: { EmbeddedChatFooter, EmbeddedChatBody, PlayIcon },
 })
-export default class EmbeddedChat extends Vue {
-  async initialize() {
-    await this.$client.initialize();
-    await this.$client.createRequest({ type: RequestType.Launch }).send();
-  }
-}
+export default class EmbeddedChat extends Vue {}
 </script>
