78146ed76ad4512e2e0682b487795cb9dffd4c69
Jan König <jankoenig120190@googlemail.com>
2020-10-26 18:16:35 +0100

    :construction: Add heading (WIP)

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div id="app" class="w-screen h-screen p-20 bg-gray-300">
+  <div id="app" class="w-screen h-screen p-16 bg-gray-300">
+    <div id="intro" class="text-center text-gray-800">
+      <h1 class="text-lg">Embedded Chat</h1>
+      <p>This <a href="https://github.com/jovotech/jovo-starter-web-embeddedchat" target="_blank">open source</a> chat is fully customizable and can be embedded into any web app.</p>
+    </div>
     <embedded-chat />
   </div>
 </template>
