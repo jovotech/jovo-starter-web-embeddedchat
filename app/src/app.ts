c915c024260408f18ca6a16e6af31597abf9834f
Jan König <jankoenig120190@googlemail.com>
2020-10-27 10:45:12 +0100

    :sparkles: Add redirect Action

--- app/src/app.ts
+++ app/src/app.ts
@@ -27,7 +27,7 @@ app.setHandler({
   LAUNCH() {
     this.ask(
       `The content of this conversation is fully customizable. ` +
-      `<a href="https://www.jovo.tech/docs/routing" target="_blank">Our docs</a> show you how to update the app logic. ` +
+      `Our docs can show you how to update the app logic. ` +
       `Do you want me to take you there?`
     ).followUpState('TakeMeToTheDocs');
     this.$webApp?.showQuickReplies([
@@ -45,10 +45,11 @@ app.setHandler({
       this.$webApp?.addActions([
         {
           type: 'CUSTOM' as any,
-          command: 'TakeMeToTheDocs',
+          command: 'redirect',
+          value: 'https://www.jovo.tech/docs/routing',
         },
       ]);
-      this.tell('Great!');
+      this.tell('Great! Opening now...');
     },
 
     Unhandled() {
--- client/src/App.vue
+++ client/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app" class="w-screen h-screen p-16 bg-gray-300">
+  <div id="app" class="w-screen h-screen p-20 bg-gray-300">
     <div id="intro" class="text-center text-gray-800">
       <h1 class="text-lg">Embedded Chat</h1>
       <p>This <a href="https://github.com/jovotech/jovo-starter-web-embeddedchat" target="_blank">open source</a> chat is fully customizable and can be embedded into any web app.</p>
@@ -10,12 +10,35 @@
 
 <script lang="ts">
 import EmbeddedChat from '@/components/EmbeddedChat.vue';
+import {
+  Action,
+  ActionType,
+  ClientEvent,
+} from 'jovo-client-web-vue';
 import { Component, Vue } from 'vue-property-decorator';
 
 @Component({
   components: { EmbeddedChat },
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+   mounted() {
+    this.$client.on(ClientEvent.Action, this.onAction);
+  }
+
+  private onAction(action: Action) {
+    if (action.type === ActionType.Custom) {
+      switch (action.command) {
+        case 'redirect': {
+          setTimeout(()=>{
+            window.open(action.value);
+          },800);
+          break;
+        }
+        default:
+      }
+    }
+  }
+}
 </script>
 
 <style>
